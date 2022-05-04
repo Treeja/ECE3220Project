a23e360f360a7f3ecbff1190bef710e2e03b3808
NEbers <nae5np@umsystem.edu>
2022-05-04 12:32:28 -0500

    Comment out code to compile

--- PlayerNotebook/PlayerNotebook.cpp
+++ PlayerNotebook/PlayerNotebook.cpp
@@ -8,29 +8,29 @@ using namespace tinyxml2;
 
 void PlayerNotebook::createNotebook(){
 
-    XMLDocument xmlDoc;
-    XMLNode * pRoot = xmlDoc.NewElement("Root");
-    xmlDoc.InsertFirstChild(pRoot);
-    XMLElement * pElement = xmlDoc.NewElement("IntValue");
-    pElement->SetText(10);
-    pRoot->InsertEndChild(pElement);
+    // XMLDocument xmlDoc;
+    // XMLNode * pRoot = xmlDoc.NewElement("Root");
+    // xmlDoc.InsertFirstChild(pRoot);
+    // XMLElement * pElement = xmlDoc.NewElement("IntValue");
+    // pElement->SetText(10);
+    // pRoot->InsertEndChild(pElement);
 
-    pElement = xmlDoc.NewElement("FloatValue");
-    pElement->SetText(0.5f);
+    // pElement = xmlDoc.NewElement("FloatValue");
+    // pElement->SetText(0.5f);
 
-    pRoot->InsertEndChild(pElement);
+    // pRoot->InsertEndChild(pElement);
 
-    pElement = xmlDoc.NewElement("Date");
-    pElement->SetAttribute("day", 26);
-    pElement->SetAttribute("month", "April");
-    pElement->SetAttribute("year", 2014);
-    pElement->SetAttribute("dateFormat", "26/04/2014");
+    // pElement = xmlDoc.NewElement("Date");
+    // pElement->SetAttribute("day", 26);
+    // pElement->SetAttribute("month", "April");
+    // pElement->SetAttribute("year", 2014);
+    // pElement->SetAttribute("dateFormat", "26/04/2014");
 
-    pRoot->InsertEndChild(pElement);
+    // pRoot->InsertEndChild(pElement);
 
-    pElement = xmlDoc.NewElement("List");
-    pRoot->InsertEndChild(pElement);
-    xmlDoc.SaveFile("SavedData.xml");
+    // pElement = xmlDoc.NewElement("List");
+    // pRoot->InsertEndChild(pElement);
+    // xmlDoc.SaveFile("SavedData.xml");
 
 }
 
